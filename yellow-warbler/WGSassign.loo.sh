70147d9d5bd05d8d77893a9301be460656714b18
marina <mdrodriguez10@gmail.com>
2023-05-22 14:29:18 -0600

    YEWA example code

---
+++ yellow-warbler/WGSassign.loo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#################
+#set a job name
+#SBATCH --job-name=WGSassign
+#SBATCH --output=wgsa.out
+#SBATCH --error=wgsa.err
+#################
+#SBATCH --time=24:00:00
+#SBATCH --ntasks-per-node=24
+#################
+
+source ~/.bashrc
+
+conda activate WGSassign
+
+BEAGLE="/WGSA_data/yewa.known.ind105.ds_2x.beagle.gz"
+POP_ID="/WGSA_data/yewa.known.ind105.reference.IDs.txt"
+
+WGSassign --beagle $BEAGLE --pop_af_IDs $POP_ID --get_reference_af --loo --out yewa.wgs.assignment --threads 20
